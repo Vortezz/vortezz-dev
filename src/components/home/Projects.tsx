02356a119ff0d80f0c6c3a2e6002bddc352b2c3b
Vortezz <contact@vortezz.dev>
2022-07-12 00:33:57 +0200

    feat: improve projects layout

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,5 +1,5 @@
 import language from "../../language";
-import ProjectsComponent from "./projects/ProjectsComponent";
+import ProjectsComponent from "./components/ProjectsComponent";
 
 // @ts-ignore
 import ekaliaLogo from "../../assets/pictures/ekalia.png";
@@ -13,10 +13,33 @@ export default function Projects() {
             <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold">{language.ABOUT.PROJECTS.TITLE}</h1>
             <div className="w-[calc(90%)] flex flex-wrap mx-auto">
                 <ProjectsComponent image={ekaliaLogo} title={"Ekalia"}
-                                   description={language.ABOUT.PROJECTS.PROJECTS.EKALIA} link={"https://ekalia.fr/"}/>
+                                   description={language.ABOUT.PROJECTS.PROJECTS.EKALIA}
+                                   links={[{
+                                       uri: "https://ekalia.fr/",
+                                       icon: "bx-link",
+                                       blank: true,
+                                   }, {
+                                       uri: "https://ekalia.fr/discord",
+                                       icon: "bxl-discord-alt",
+                                       blank: true,
+                                   }, {
+                                       uri: "https://ekalia.fr/twitter",
+                                       icon: "bxl-twitter",
+                                       blank: true,
+                                   }]}
+                />
                 <ProjectsComponent image={molkkyLogo} title={"Möllky Count"}
                                    description={language.ABOUT.PROJECTS.PROJECTS.MOLKKY}
-                                   link={"https://github.com/Vortezz/molkky-count"}/>
+                                   links={[{
+                                       uri: "https://github.com/Vortezz/molkky-count",
+                                       icon: "bxl-github",
+                                       blank: true,
+                                   }, {
+                                       uri: "https://play.google.com/store/apps/details?id=dev.vortezz.molkkycount",
+                                       icon: "bxl-play-store",
+                                       blank: true,
+                                   }]}
+                />
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,12 +25,12 @@ export default function Home() {
     return (
         <div className="h-screen w-full bg-vortezz-gray2 min-height-full">
             <Navbar/>
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-extrabold text-vortezz-white text-center mt-6 group">👋 {language.ABOUT.WELCOME.HEY}
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-extrabold text-vortezz-white text-center mt-6 group">👋 {language.ABOUT.WELCOME.HEY}&nbsp;
                 <span className="group-hover:text-vortezz-purple">Vortezz</span>,</h1>
-            <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-vortezz-white text-center mt-2 group">{language.ABOUT.WELCOME.PREFIX}
+            <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-vortezz-white text-center mt-2 group">{language.ABOUT.WELCOME.PREFIX}&nbsp;
                 <span className="group"><span className="group-hover:text-social-discord">{frenchArray[0]}</span><span
                     className="group-hover:text-vortezz-white">{frenchArray[1]}</span><span
-                    className="group-hover:text-social-youtube">{frenchArray[2]}</span></span> {language.ABOUT.WELCOME.SUFFIX}.
+                    className="group-hover:text-social-youtube">{frenchArray[2]}</span></span>&nbsp;{language.ABOUT.WELCOME.SUFFIX}.
             </h2>
             {Wave(1)}
             <Who/>
